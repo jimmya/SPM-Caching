ada35225882d2d44e0196a0abcc839d69c10fdbe
Jimmy Arts <jimmyarts@polarsteps.com>
2023-07-17 17:39:52 +0200

    Initial commit

---
+++ Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Jimmy Arts on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
